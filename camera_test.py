77160b282089335d2835c2d2dcd15084487b54ec
WolfRayet-102 <pattriciaruth@gmail.com>
2026-01-15 16:54:50 +0800

    fixed and completed the spriint 1

--- camera_test.py
+++ camera_test.py
@@ -1,6 +1,10 @@
+import time
 import cv2 # type: ignore
 
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+prev_time = time.time()
 
 while True:
     ret, frame = cap.read()
@@ -8,9 +12,22 @@ while True:
         print("Failed to grab frame")
         break
 
-    cv2.imshow("Camera Test", frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)     
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    current_time= time.time()
+    fps = 1/(current_time-prev_time)
+    prev_time = current_time
+
+    h, w = frame.shape[:2]
+    cv2.putText(gray, f"{w}x{h}", (10,60),
+                cv2.FONT_HERSHEY_TRIPLEX, 0.8, (255,0,0), 2)
+
+    cv2.putText(gray, f"FPS: {fps:.2f}", (10,30),
+            cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+
+    cv2.imshow("Camera Test", gray)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 cap.release()
